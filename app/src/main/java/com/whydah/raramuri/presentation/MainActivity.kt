ed98bc7fa23896dcd35dddd879eb798067caf089
Duy Bui <duy.bui@kardiachain.io>
2022-11-11 11:25:57 +0700

    First init

---
+++ app/src/main/java/com/whydah/raramuri/presentation/MainActivity.kt
@@ -0,0 +1,95 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.whydah.raramuri.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.whydah.raramuri.R
+import com.whydah.raramuri.presentation.theme.RaramuriWearTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WearApp("Android")
+        }
+    }
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    RaramuriWearTheme {
+        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+         * version of LazyColumn for wear devices with some added features. For more information,
+         * see d.android.com/wear/compose.
+         */
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(bottom = 10.dp)) {
+                    Text(text = "Walk")
+                }
+            }
+            item {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(bottom = 10.dp)) {
+                    Text(text = "Walk")
+                }
+            }
+            item {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(bottom = 10.dp)) {
+                    Text(text = "Bike")
+                }
+            }
+
+            item {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(bottom = 10.dp)) {
+                    Text(text = "Run")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world, greetingName)
+    )
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
